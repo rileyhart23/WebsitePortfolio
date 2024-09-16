5938ce55c8e54f16995903f2053fc6d40f792580
rileyhart23 <riley.wasdyke@icloud.com>
2024-09-15 20:06:04 -0400

    AboutMe Page

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ menu.addEventListener('click', function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+document.querySelector('a[href="#getToKnowMore"]').addEventListener('click', function (event) {
+    event.preventDefault();
+    window.scrollTo({
+        top: document.querySelector('#getToKnowMore').offsetTop - 100, // Adjust offset value as needed
+        behavior: 'smooth'
+    });
+});
