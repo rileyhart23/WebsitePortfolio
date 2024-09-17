1e9d96b5d5ad928a185cc96a5cf222ea130adb53
rileyhart23 <riley.wasdyke@icloud.com>
2024-09-17 11:24:09 -0400

    formatting code

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
-menu.addEventListener('click', function () {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
 });
 
-document.querySelector('a[href="#getToKnowMore"]').addEventListener('click', function (event) {
+document
+  .querySelector('a[href="#getToKnowMore"]')
+  .addEventListener("click", function (event) {
     event.preventDefault();
     window.scrollTo({
-        top: document.querySelector('#getToKnowMore').offsetTop - 100, // Adjust offset value as needed
-        behavior: 'smooth'
+      top: document.querySelector("#getToKnowMore").offsetTop - 100, // Adjust offset value as needed
+      behavior: "smooth",
     });
-});
+  });
